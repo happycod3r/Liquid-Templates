4b805238ecba81554945098be38544ffeea6c2fa
happycod3r <happy2code0101@gmail.com>
2023-08-13 17:44:21 -0400

    [chore] Updated repo_template.[py, md]

--- repo_template.md
+++ repo_template.md
@@ -1,4 +1,25 @@
-# Welcome to {{ repo_title }}
+# {{ repo_title }}
+{{ repo_summary }}
 {% for section in sections %}
-  ## {{ section.section_header }}
+## [{{ section.section_header }}](#)
+{{ section.section_summary }}
+
+{% if section.section_header == "Contributing" %}
+If you have any feature requests, suggestions or general questions you can reach 
+me via any of the methods listed below in the [Contacts](#contacts) section.
+{% endif %}
+
+{% if section.section_header == "Security" %}
+### Reporting a vulnerability or bug?
+Do not submit an issue or pull request: A general rule of thumb is to never publicly report bugs or vulnerabilities because you might inadvertently reveal it to unethical people who may use it for bad. Instead, you can email me directly at: [paulmccarthy676@gmail.com](mailto:paulmccarthy676@gmail.com). I will deal with the issue privately and submit a patch as soon as possible.
+{% endif %}
+    
+{% if section.section_header == "Contacts" %}
+**Author:** Paul M.
+- Email: [paulmccarthy676@gmail.com](mailto:paulmccarthy676@gmail.com)
+- Github: [https://github.com/happycod3r](https://github.com/happycod3r)
+- Linkedin: [https://www.linkedin.com/in/paul-mccarthy-89165a269/](https://www.linkedin.com/in/paul-mccarthy-89165a269/)
+- Facebook: [https://www.facebook.com/paulebeatz](https://www.facebook.com/paulebeatz)
+{% endif %}
+
 {% endfor %}
--- repo_template_renderer.py
+++ repo_template_renderer.py
@@ -1,33 +1,75 @@
 from liquid import Liquid
 
 
-def render_liquid_md(file_name: str, repo_title: str, repo_sections: dict) -> None:
-    title = repo_title
-    sections = repo_sections
-
-    with open(file_name, "r") as template_file:
+def render_liquid_md(
+    template_file_name: str,
+    rendered_file_name: str,
+    repo_title: str, 
+    repo_summary: str, 
+    repo_sections: dict) -> None:
+    
+    def write_rendered_content(content):
+        try:
+            with open(rendered_file_name, 'w') as file:
+                file.write(content)
+                file.close()
+        except FileNotFoundError as e:
+            print(repr(e))
+        
+    with open(template_file_name, "r") as template_file:
         template = template_file.read()
         
-    liquid = Liquid(file_name)
+    liquid = Liquid(template_file_name)
 
     rendered_content = liquid.render(
-        repo_title=title, 
-        sections=sections
+        repo_title=repo_title,
+        repo_summary=repo_summary, 
+        sections=repo_sections
     )
 
     template_file.close()
-    print(rendered_content)
+    write_rendered_content(rendered_content)
     
 render_liquid_md(   
-    "repo_template.md",
-    "Test",
-    [
-        {"section_header": "Table Of Contents"},
-        {"section_header": "About"},
-        {"section_header": "Install"},
-        {"section_header": "Usage"},
-        {"section_header": "Contributing"},
-        {"section_header": "Security"},
-        {"section_header": "Contacts"}, 
+    template_file_name="repo_template.md",
+    rendered_file_name="rendered/TEST.md",
+    repo_title="Test",
+    repo_summary="A new test repository for liquid markdown templates",
+    repo_sections=[
+        {
+            "section_header": "Table Of Contents",
+            "section_summary": "The toc section",
+            "section_image": None,
+        },
+        {
+            "section_header": "About",
+            "section_summary": "The about section",
+            "section_image": None,
+        },
+        {
+            "section_header": "Install",
+            "section_summary": "The install section",
+            "section_image": None,
+        },
+        {
+            "section_header": "Usage",
+            "section_summary": "The usage section",
+            "section_image": None,
+        },
+        {
+            "section_header": "Contributing",
+            "section_summary": "The contributing section",
+            "section_image": None,
+        },
+        {
+            "section_header": "Security",
+            "section_summary": "The security section",
+            "section_image": None,
+        },
+        {
+            "section_header": "Contacts",
+            "section_summary": "The contacts section",
+            "section_image": None,
+        },
     ]
 )
